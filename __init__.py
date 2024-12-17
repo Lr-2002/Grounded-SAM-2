7c0a10d2ce40a58db363a3aca5eb35da82426bf9
Lr_2002 <2629651228@qq.com>
2024-12-17 23:00:51 +0800

    feat: fix online_process_two_videos

--- __init__.py
+++ __init__.py
@@ -5,3 +5,4 @@ print(os.path.dirname(os.path.abspath(__file__)))
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
 from .video_processor import VideoProcessor
+from .online_process_two_videos import MaskGenerator
